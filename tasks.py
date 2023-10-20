2e5eb3f0e7a4978bf393a3ff6e545c83c2c2567c
Mikko Korpela <mikko@robocorp.com>
2023-10-20 13:10:44 +0300

    second draft

--- tasks.py
+++ tasks.py
@@ -1,19 +1,20 @@
 from ast import List
-from typing import Any, Dict, Iterator
+from typing import Iterator
+import pypdf
 from robocorp.tasks import task
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
+from langchain.llms.openai import OpenAI
+from langchain.schema import Document
 from langchain.schema.messages import (
     HumanMessage,
     AIMessage,
     SystemMessage,
-    BaseMessageChunk,
     BaseMessage,
 )
-from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores.chroma import Chroma
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from robocorp.log import console_message
 from langchain.chains import ConversationalRetrievalChain, LLMChain
 from langchain.chains.question_answering import load_qa_chain
@@ -29,24 +30,24 @@ load_dotenv()
 
 
 @task
-def run_chat():
-    chat = ChatOpenAI()
-    messages: List[BaseMessage] = [
-        SystemMessage(content="You're a helpful assistant."),
-        HumanMessage(content="Make a poem about a digital AI assistant called ReMark."),
-    ]
-    _write_stream_to_console(chat.stream(messages))
+def run_llm():
+    llm = OpenAI()
+    start = """
+A long, long time ago
+In a galaxy
+""".lstrip()
+    _write(start)
+    _write_stream_to_console(llm.stream(start, stop="\n"))
 
 
 @task
-def run_chat_and_see_response():
+def run_chat_llm_and_see_response():
     chat = ChatOpenAI()
     messages: List[BaseMessage] = [
         SystemMessage(content="You're a helpful assistant."),
         HumanMessage(content="Make a poem about a digital AI assistant called ReMark."),
     ]
-    result = chat.generate([messages])
-    print(result)
+    chat.generate([messages])
 
 
 @task
@@ -54,31 +55,38 @@ def run_chat_interactive():
     chat = ChatOpenAI()
     messages: List[BaseMessage] = [SystemMessage(content="You're a helpful assistant.")]
     while True:
-        console_message("Human > ", "", flush=True)
+        _write("Human > ")
         human_message = input()
         if not human_message.strip():
             break
         messages.append(HumanMessage(content=human_message))
-        console_message("AI > ", "", flush=True)
-        result = _write_stream_to_console(chat.stream(messages))
+        _write("AI > ")
+        result = _write_stream_to_console(c.content for c in chat.stream(messages))
         messages.append(AIMessage(content=result))
-    console_message("\n<END OF DISCUSSION>\n", "", flush=True)
+    _write("\n<END OF DISCUSSION>\n")
 
 
 @task
 def load_document_embeddings_to_database():
-    loader = UnstructuredFileLoader("./RobotFrameworkUserGuide.html", mode="elements")
-    raw_documents = loader.load()
-    for doc in raw_documents:
-        doc.metadata = {"source": "Robot Framework User Guide"}
-    # split it into chunks
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    splitted_documents = text_splitter.split_documents(raw_documents)
+    source = "ICC-Men-s-CWC23-Playing-Conditions-single-pages.pdf"
+    reader = pypdf.PdfReader(source)
+    docs = (
+        Document(
+            page_content=page.extract_text(),
+            metadata={
+                "source": source,
+                "page": page_number,
+            },
+        )
+        for page_number, page in enumerate(reader.pages, start=1)
+    )
+    text_splitter = RecursiveCharacterTextSplitter()
+    splitted_documents = text_splitter.split_documents(docs)
     db = Chroma.from_documents(
         splitted_documents, OpenAIEmbeddings(), persist_directory=".chroma"
     )
     # query it
-    query = "What does CURDIR do?"
+    query = "Duckworth-Lewis-Stern table"
     data = db.similarity_search(query)
 
     # print results
@@ -87,50 +95,61 @@ def load_document_embeddings_to_database():
 
 @task
 def rag_bot():
-    verbose = False
+    verbose = True
     db = Chroma(embedding_function=OpenAIEmbeddings(), persist_directory=".chroma")
-    llm = ChatOpenAI()
     combine_docs_chain = load_qa_chain(
-        llm=llm,
+        llm=ChatOpenAI(model="gpt-3.5-turbo-16k"),
         prompt=_answering_prompt(),
         verbose=verbose,
         document_prompt=_document_prompt(),
     )
     question_generator_chain = LLMChain(
-        llm=llm, prompt=_data_query_generating_prompt(), verbose=verbose
+        llm=ChatOpenAI(), prompt=_data_query_generating_prompt(), verbose=verbose
     )
     chain = ConversationalRetrievalChain(
         combine_docs_chain=combine_docs_chain,
-        retriever=db.as_retriever(),
+        retriever=db.as_retriever(search_kwargs={"k": 10}),
         question_generator=question_generator_chain,
         verbose=verbose,
     )
     messages: List[BaseMessage] = []
     while True:
-        console_message("Human > ", "", flush=True)
+        _write("Human > ")
         human_message = input()
         if not human_message.strip():
             break
-        console_message("AI > ", "", flush=True)
+        _write("AI > ")
         result = chain({"question": human_message, "chat_history": messages})
-        console_message(f"{result['answer']}\n", "", flush=True)
+        _write(f"{result['answer']}\n")
         messages.append(HumanMessage(content=human_message))
         messages.append(AIMessage(content=result["answer"]))
-    console_message("\n<END OF DISCUSSION>\n", "", flush=True)
+    _write("\n<END OF DISCUSSION>\n")
 
 
 def _document_prompt() -> PromptTemplate:
     return PromptTemplate.from_template(
-        """Content source: {source}\n{page_content}\n"""
+        "Content source: {source}\npage: {page}\n{page_content}\n"
     )
 
 
-def _answering_prompt() -> PromptTemplate:
-    system_template = """Use the following pieces of content to answer the users question. 
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
+def _answering_prompt() -> ChatPromptTemplate:
+    system_template = """
+For the question provided, consider the context pieces listed in the Content section for generating your answer, but only if they seem relevant.
+Your answer should rely exclusively on the information provided in the Content section.
+Clearly state if the Content section does not contain sufficient information to formulate a response to the question.
+Ensure that your answer is clear, coherent, and accurately represents the information from the Content.
+
 ----------------
-#Content
-{context}"""
+#Content section:
+
+{context}
+----------------
+Remember to direct address the User's question. Avoid unnecessary repetition and keep answers concise and compact.
+Default to use the natural language the User is using in the answer.
+
+For easy user cross-verification, include inline citations using the modified APA approach, 
+placing the Content source and page number in parentheses immediately after the referenced information or at the end of each paragraph.
+""".lstrip()
     messages = [
         SystemMessagePromptTemplate.from_template(system_template),
         HumanMessagePromptTemplate.from_template("{question}"),
@@ -140,16 +159,23 @@ If you don't know the answer, just say that you don't know, don't try to make up
 
 def _data_query_generating_prompt() -> PromptTemplate:
     return PromptTemplate.from_template(
-        """Combine the chat history and follow up question into
+        """
+You are a content gathering assistant for ICC Men's Cricket World Cup 2023 information retrieval.
+Combine the chat history and follow up question into
 a standalone question. Chat History: {chat_history}
-Follow up question: {question}"""
+Follow up question: {question}
+""".lstrip()
     )
 
 
-def _write_stream_to_console(stream: Iterator[BaseMessageChunk]) -> str:
-    full_message: List[str] = []
+def _write_stream_to_console(stream: Iterator[str]) -> str:
+    full_message: list[str] = []
     for chunk in stream:
-        console_message(chunk.content, "User messages", flush=True)
-        full_message.append(chunk.content)
-    console_message("\n", "User messages", flush=True)
+        _write(chunk)
+        full_message.append(chunk)
+    _write("\n")
     return "".join(full_message)
+
+
+def _write(message: str):
+    console_message(message, "stdout", flush=True)
